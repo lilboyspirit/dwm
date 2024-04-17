1c0740990601138285e7c7938293c21049b42fb0
Spirit <spirit@programmer.net>
2024-04-17 23:29:29 +0300

    code at tag 4 and firefox at 5

--- config.def.h
+++ config.def.h
@@ -26,9 +26,9 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class         instance    title       tags mask     isfloating   monitor */
+	{ "Code - OSS",  NULL,       NULL,       1 << 3,       0,           -1 },
+	{ "Firefox",     NULL,       NULL,       1 << 4,       0,           -1 },
 };
 
 /* layout(s) */
